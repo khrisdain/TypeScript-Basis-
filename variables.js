54056b18f9ee76cba660ab0d51ed343d08fb252b
khrisdain <samuelilesanmi15@gmail.com>
2023-01-29 21:47:46 +0100

    Typescript functions

---
+++ variables.js
@@ -0,0 +1,19 @@
+// let greetings: string = "Hello Olamide";
+// console.log(greetings);
+// //Nb Declaration of greeting types as a string grants access to string properties and restrict other non string properties 
+// export {} //export an object
+// //number
+// let userId: number = 334455 //annotates userId is number
+// userId.toFixed
+// //Boolean
+// let isLoggedIn: boolean = false
+// function addTwo(num) {
+//     return num + 2;
+// }
+// console.log(addTwo(5));
+
+function getUpper(val) {
+    val.toUpperCase();
+}
+let res = getUpper("olanrewaju");
+console.log(res)
