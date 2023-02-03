bd154723cfb46dc436f4d0efcc82083b150651b9
khrisdain <samuelilesanmi15@gmail.com>
2023-02-03 22:35:17 +0100

    Today commit

--- variables.js
+++ variables.js
@@ -1,5 +1,15 @@
-const loginUser = (name: string, email: string, isLogin: Boolean) => {
-    return isLogin
-}
+"use strict";
+exports.__esModule = true;
+var getHello = function (s) {
+    return "";
+};
 
-loginUser("samuel", "s@gmail.com",)
+var heroes = ["thor", "spiderman", "ironman"];
+
+const Hero = heroes.map((hero) =>  {
+    return "Hero is ".concat(hero);
+});
+
+console.log(Hero)
+
+
--- variables.ts
+++ variables.ts
@@ -1,46 +1,12 @@
-    // let greetings: string = "Hello Olamide";
-
-    // console.log(greetings);
-
-    // //Nb Declaration of greeting types as a string grants access to string properties and restrict other non string properties 
-
-    // export {} //export an object
-
-    // //number
-    // let userId: number = 334455 //annotates userId is number
-
-    // userId.toFixed
-
-    // //Boolean
-    // let isLoggedIn: boolean = false
-
-// function addTwo(num: number){
-//     return num + 2
-// }
-
-// console.log(addTwo(5))
-
-// function getUpper(val: string){
-//     val.toUpperCase()
-// }
-
-// getUpper("olanrewaju")
-
-// function signUpUser(name: string, email: string, password: string){
-//     return signUpUser(name); 
-// };
-
-// signUpUser('olamide', 'joseph14@gmail.com', 'olamide16');
-
-
-// function logInUser(name: string, isLogin: boolean, email: null){
-//     return isLogin;
-// }
-
-// logInUser();
-
-var loginUser = (name: string, email: string, isLogin: Boolean = false) => { //after annotation default values can be set
-    return isLogin
+const getHello = (s: string): string => {
+    return ""
 }
 
-loginUser("samuel", "s@gmail.com",)
+const heroes = [ "thor", "spiderman", "ironman"]
+
+heroes.map((hero) => {
+    return `Hero is ${hero}`
+})
+
+console.log(heroes)
+export {}
